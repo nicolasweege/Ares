f5ca82cbadac8d79e842a591e830feb4d5b1055d
Nícolas <nickweege25@gmail.com>
2022-02-06 11:30:06 -0300

    Add PlayerInputActions asset

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+
+    [SerializeField] private GameObject playerShotPrefab;
+    private Rigidbody2D playerRb2D;
+    private PlayerInputActions playerInputActions;
+
+    private void Start()
+    {
+        playerRb2D = GetComponent<Rigidbody2D>();
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    private void Update()
+    {
+
+    }
 }
