d1a00cb368b5046fb94d2a7caf2cac5ca37f7f62
Nícolas <nickweege25@gmail.com>
2022-02-28 22:44:49 -0300

    Add a graphics menu option to the video submenu

--- Assets/_Game/_Scripts/UI Scripts/SettingsMenu.cs
+++ Assets/_Game/_Scripts/UI Scripts/SettingsMenu.cs
@@ -2,22 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer MainMixer;
+    [SerializeField] private AudioMixer _mainMixer;
+    private Resolution[] _resolutions;
+    public Dropdown ResolutionsDropdown;
+
+    private void Start() {
+        _resolutions = Screen.resolutions;
+        ResolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + "x" + _resolutions[i].height;
+            options.Add(option);
+        }
+
+        ResolutionsDropdown.AddOptions(options);
+    }
+
     public void SetMasterVolume(float masterVolume)
     {
-        MainMixer.SetFloat("MasterVolume", masterVolume);
+        _mainMixer.SetFloat("MasterVolume", masterVolume);
+        _mainMixer.SetFloat("SoundsVolume", masterVolume);
+        _mainMixer.SetFloat("MusicVolume", masterVolume);
     }
 
     public void SetSoundsVolume(float soundsVolume)
     {
-        MainMixer.SetFloat("SoundsVolume", soundsVolume);
+        _mainMixer.SetFloat("SoundsVolume", soundsVolume);
     }
 
     public void SetMusicVolume(float musicVolume)
     {
-        MainMixer.SetFloat("MusicVolume", musicVolume);
+        _mainMixer.SetFloat("MusicVolume", musicVolume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
