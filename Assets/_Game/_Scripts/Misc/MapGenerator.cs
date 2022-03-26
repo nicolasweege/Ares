72f829a1bd0306fa0e9d322150d749d1f8dd8ea2
Nícolas <nickweege25@gmail.com>
2022-03-26 18:38:34 -0300

    Add a specific parameter to Portal and Key's generation

--- Assets/_Game/_Scripts/Misc/MapGenerator.cs
+++ Assets/_Game/_Scripts/Misc/MapGenerator.cs
@@ -8,7 +8,7 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private GameObject _key;
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _camera;
-    [SerializeField] private const int _mapHight = 32;
+    [SerializeField] private const int _mapHight = 64;
     [SerializeField] private const int _mapWidth = 64;
 
     private void Start()
@@ -28,18 +28,24 @@ public class MapGenerator : MonoBehaviour
 
     private void CreateElements()
     {
-        Vector3 portalPos = GetElementPos();
-        Vector3 keyPos = GetElementPos();
-        Vector3 playerPos = GetElementPos();
+        Vector3 portalPos = GetPortalPos();
+        Vector3 keyPos = GetKeyPos();
+        Vector3 playerPos = new Vector3(0f, 0f, 0f);
 
         var portal = Instantiate(_portal, portalPos, transform.rotation);
         var key = Instantiate(_key, keyPos, transform.rotation);
+
         var player = Instantiate(_player, playerPos, transform.rotation);
         _camera.GetComponent<FollowingCamera>().SetPlayerTransform(player.transform);
     }
 
-    private Vector3 GetElementPos()
+    private Vector3 GetPortalPos()
     {
-        return new Vector3(Random.Range(-_mapWidth, _mapWidth), Random.Range(-_mapHight, _mapHight), 0f);
+        return new Vector3(Random.Range(-_mapWidth*.9f, -_mapWidth*.8f), Random.Range(-_mapHight*.9f, _mapHight*.9f), 0f);
+    }
+
+    private Vector3 GetKeyPos()
+    {
+        return new Vector3(Random.Range(_mapWidth*.9f, _mapWidth*.8f), Random.Range(-_mapHight*.9f, _mapHight*.9f), 0f);
     }
 }
