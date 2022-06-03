d51d404043911f93aaf91aaf0e6c0a38dfa0df62
Nícolas <nickweege25@gmail.com>
2022-06-02 22:09:02 -0300

    Add the Enemy_2_Map_1 and its Shot prefabs

---
+++ Assets/_Game/_Scripts/Enemies/Enemy 2 Map 1/Enemy_2_Map_1_Shot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_2_Map_1_Shot : Shot
+{
+    private Rigidbody2D _rb;
+
+    private void Start() => _rb = GetComponent<Rigidbody2D>();
+
+    private void Update() => DeactiveShot();
+}
