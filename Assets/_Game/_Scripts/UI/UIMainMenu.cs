72aa2c603866013cd7da30dac58606f496740400
Nícolas <nickweege25@gmail.com>
2022-08-07 15:44:55 -0300

    .

--- Assets/_Game/_Scripts/UI/UIMainMenu.cs
+++ Assets/_Game/_Scripts/UI/UIMainMenu.cs
@@ -4,14 +4,33 @@ using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
-    public void OpenMenu()
+    [SerializeField] private float _leanTweenDuration;
+    [SerializeField] private GameObject _mainMenuComponents, _optionsMenuComponents;
+
+    private void Awake()
     {
-        // transform.LeanMove(new Vector2())
+        LeanTween.reset();
     }
 
-    public void CloseMenu()
+    public void OpenMainMenu()
     {
+        LeanTween.moveLocal(_mainMenuComponents, new Vector3(0, 0, 0), _leanTweenDuration).setEaseOutExpo();
+    }
 
+    public void CloseMainMenu()
+    {
+        LeanTween.moveLocal(_mainMenuComponents, new Vector3(-1200, 0, 0), _leanTweenDuration).setEaseInExpo().setOnComplete(OpenOptionsMenu);
+    }
+
+    public void OpenOptionsMenu()
+    {
+        LeanTween.moveLocal(_optionsMenuComponents, new Vector3(0, 0, 0), _leanTweenDuration).setEaseOutExpo();
+        LeanTween.moveLocal(_optionsMenuComponents, new Vector3(0, 10f, 0), _leanTweenDuration).setDelay(0.25f);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        LeanTween.moveLocal(_optionsMenuComponents, new Vector3(1200, 0, 0), _leanTweenDuration).setEaseInExpo().setOnComplete(OpenMainMenu);
     }
 
     public void PlayGame() => SceneManager.LoadScene("Afrodite Fight");
