1cb5a2f18c69a0017bb17abd646ae2af9e0de2ff
Nícolas <nickweege25@gmail.com>
2022-08-07 13:05:21 -0300

    .

--- Assets/_Game/_Scripts/UI/UIMainMenu.cs
+++ Assets/_Game/_Scripts/UI/UIMainMenu.cs
@@ -4,6 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
+    public void OpenMenu()
+    {
+        // transform.LeanMove(new Vector2())
+    }
+
+    public void CloseMenu()
+    {
+
+    }
+
     public void PlayGame() => SceneManager.LoadScene("Afrodite Fight");
 
     public void QuitGame()
