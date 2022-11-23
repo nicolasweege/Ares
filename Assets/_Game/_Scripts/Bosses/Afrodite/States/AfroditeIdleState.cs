f74c64f26ca956760bce03578b2668239884687b
Nícolas <machadonicolasweege@gmail.com>
2022-11-23 20:52:24 -0300

    .

--- Assets/_Game/_Scripts/Bosses/Afrodite/States/AfroditeIdleState.cs
+++ Assets/_Game/_Scripts/Bosses/Afrodite/States/AfroditeIdleState.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class AfroditeIdleState : AfroditeBaseState {
-    private float _timeToSwitchState = 1f;
+    private float _timeToSwitchState = 2f;
     private float _timer;
 
     public override void EnterState(AfroditeController context) {
